ba24c35d3c7e17132aac50c1d57d2e3d2876292a
calgray <calgray@users.noreply.github.com>
2021-11-22 08:17:34 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ICRAR"
+original_name="daliuge_component_nifty"
+original_urlname="daliuge-component-nifty"
+original_description="Awesome daliuge_component_nifty created by ICRAR"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv daliuge_component_nifty $name
 
 # remove the template related documents
 rm ABOUT_THIS_TEMPLATE.md
--- tests/test_components.py
+++ tests/test_components.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project_name import MyAppDROP, MyDataDROP
+from daliuge_component_nifty import MyAppDROP, MyDataDROP
 
 given = pytest.mark.parametrize
 
